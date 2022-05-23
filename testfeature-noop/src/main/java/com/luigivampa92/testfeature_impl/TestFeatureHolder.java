0cd1a56bc6807c5aa8a420ca17368905eff4a0a7
Pavel Vasilev <vasilevp@vtb.ru>
2022-05-23 18:03:36 +0300

    Remove nonnulls annotations in java code of noop module

--- testfeature-noop/src/main/java/com/luigivampa92/testfeature_impl/TestFeatureHolder.java
+++ testfeature-noop/src/main/java/com/luigivampa92/testfeature_impl/TestFeatureHolder.java
@@ -1,14 +1,11 @@
 package com.luigivampa92.testfeature_impl;
 
-//import androidx.annotation.NonNull;
-
 import com.luigivampa92.testfeature_api.TestFeatureApi;
 import com.luigivampa92.testfeature_api.TestFeatureAvailabilityChecker;
 import com.luigivampa92.testfeature_api.TestFeatureLauncher;
 
 public class TestFeatureHolder implements TestFeatureApi {
 
-//    @NonNull
     @Override
     public TestFeatureAvailabilityChecker getAvailabilityChecker() {
         return new TestFeatureAvailabilityChecker() {
@@ -19,7 +16,6 @@ public class TestFeatureHolder implements TestFeatureApi {
         };
     }
 
-//    @NonNull
     @Override
     public TestFeatureLauncher getFeatureLauncher() {
         return new TestFeatureLauncher() {
