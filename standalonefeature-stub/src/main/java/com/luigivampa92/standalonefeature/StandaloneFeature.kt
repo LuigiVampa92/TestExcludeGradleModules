2b484e2fbd2dc9b1e1f97de06dcb3e9776ea5b70
Pavel Vasilev <vasilevp@vtb.ru>
2022-05-24 22:12:35 +0300

    Fixed build problem for standalone module

--- standalonefeature-stub/src/main/java/com/luigivampa92/standalonefeature/StandaloneFeature.kt
+++ standalonefeature-stub/src/main/java/com/luigivampa92/standalonefeature/StandaloneFeature.kt
@@ -1,6 +1,8 @@
 package com.luigivampa92.standalonefeature
 
-class StandaloneFeature {
+import com.luigivampa92.core.FeatureApi
+
+class StandaloneFeature : FeatureApi {
 
     fun getStandaloneFeatureString() = "I am fake standalone feature stub - real feature was excluded from the build"
 
