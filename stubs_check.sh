18f92ae9e748d9804c4149adc61d80e2da243b72
Pavel Vasilev <vasilevp@vtb.ru>
2022-05-26 10:07:43 +0300

    Added docs to config files and check script

--- stubs_check.sh
+++ stubs_check.sh
@@ -1,12 +1,22 @@
+# This is a stubs check script. Not very effective really, should be replaced with better approach later.
+# It runs a task to compile the code in stubs to make sure it is not broken and up to date.
+# Beware that it cannot check the code on the calling side in other projects.
+# The task is picked the way it has no time consuming dependencies and will take as little time as possible.
+#
+# This file is intended to be stored under VCS.
+
+gradle_task_for_a_check="compileDebugSources"
+
 if ! grep -q "stubs.enabled=true" "local.properties"; then
   echo "ERROR - NOT IN A STUB MODE"
   exit 1
 else
-  gradle_task="compileDebugSources"
   gradle_projects=""
-  while read line; do gradle_projects="$gradle_projects$line:$gradle_task "; done < stubs_available.conf
+  while read line; do
+    if [[ "$line" == ":"* ]]; then
+      gradle_projects="$gradle_projects$line:$gradle_task_for_a_check ";
+    fi
+  done < stubs_available.conf
   gradle_exec="./gradlew $gradle_projects"
-#  echo "$gradle_exec"
-#  gradle_exec_res=`eval "$gradle_exec"`
   eval "$gradle_exec"
 fi
